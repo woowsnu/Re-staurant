07764db89a0158b1dd8789f9940e1210b8d2b88b
deeun <jiwon94k@gmail.com>
2022-09-01 18:53:02 +0900

    Design : common UI 제작

--- front/src/component/MyPage/EditProfile.js
+++ front/src/component/MyPage/EditProfile.js
@@ -4,12 +4,15 @@ import Button from "../UI/Button";
 import Uploader from "./Uploader";
 import axios from "../../api/axios";
 import Modal from "../UI/Modal";
+import colors from "../../styles/colors"
+import Input from "../UI/Input";
 
 const EditProfile = (props) => {
   const [nickname, setNickname] = useState("");
 
   const nicknameChangeHandler = (e) => {
     e.preventDefault();
+    console.log(e.target.value);
     setNickname(e.target.value);
   };
 
@@ -51,15 +54,15 @@ const EditProfile = (props) => {
       <Uploader/>
       <br/><br/>
       <label htmlFor="nickname">닉네임</label>
-      <input
+      <Input
         id="nickname"
         type="text"
         value={nickname}
         onChange={nicknameChangeHandler}
       />
       <br/><br/><br/>
-      <Button type="submit" onClick={saveNewProfileHandler}>저장하기</Button>
-      <Button onClick={closeModal}>닫기</Button>
+      <Button style={{backgroundColor : `${colors.primary2}`}} type="submit" onClick={saveNewProfileHandler}>저장하기</Button>
+      <Button style={{backgroundColor : `${colors.primary2}`}} onClick={closeModal}>닫기</Button>
     </Modal>
   );
 };
