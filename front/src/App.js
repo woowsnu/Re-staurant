06414745a172ad412a7608db6cb1cae08b381c2e
woowsnu <woowsnu@gmail.com>
2022-09-02 17:30:34 +0900

    Feat : 메인 경로 컴포넌트 추가
    - Homepage 컴포넌트에 Header, Main, Footer 추가

--- front/src/App.js
+++ front/src/App.js
@@ -7,23 +7,25 @@ import ProtectedRoutes from "./routes/ProtectedRoutes";
 import ProtectedRoutesAuth from "./routes/ProtectedRoutesAuth";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import ReviewWrite from "./component/Review/ReviewWrite";
+import HomePage from "./routes/HomePage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-        <Route path="/detail/:id" element={<RestaurantDetailPage />} />
-        <Route element={<ProtectedRoutesAuth />}>
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/members" element={<AuthPage />} />
-        </Route>
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/review/write/:id" element={<ReviewWrite />} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<App />} />
+          <Route path="/detail/:id" element={<RestaurantDetailPage />} />
+          <Route element={<ProtectedRoutesAuth />}>
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/members" element={<AuthPage />} />
+          </Route>
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/review/write/:id" element={<ReviewWrite />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
