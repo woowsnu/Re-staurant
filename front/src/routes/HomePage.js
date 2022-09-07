ea2222d3d9bd105d9a22dd67851e864a5612af05
woowsnu <woowsnu@gmail.com>
2022-09-07 11:18:01 +0900

    Feat : HomePage Tags 영역 적용
    - 디자인 수정 필요

--- front/src/routes/HomePage.js
+++ front/src/routes/HomePage.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React from 'react';
 import Layout from '../component/Layout/Layout';
 import Header from '../component/Layout/Header';
 import Tag from '../component/UI/Tag';
 import PhotoCard from '../component/UI/PhotoCard';
+import styles from './HomePage.module.css';
+
+const TAG = ['삼겹살', '우동', '카레', '까눌레', '중식'];
 
 const HomePage = () => {
   return (
     <Layout>
       <Header />
-      <div>
-        <Tag />
-        <Tag />
-        <Tag />
-        <Tag />
+      <div className={styles.tags}>
+        {TAG.map((item, i) => {
+          return <Tag key={i} name={item} />;
+        })}
       </div>
       <div>
         <h3>맛 보장! 재방문 많은 맛집</h3>
