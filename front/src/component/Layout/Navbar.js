a8b5380f5932ae3147259f1823d43f502985a552
woowsnu <woowsnu@gmail.com>
2022-09-07 10:56:49 +0900

    Feat : Navbar 구현
    - 로그인 여부에 따라 로그인, 마이페이지로 이동 버튼
    - css 수정 필요 (justify-content 안먹음)

--- front/src/component/Layout/Navbar.js
+++ front/src/component/Layout/Navbar.js
@@ -1,8 +1,30 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
+import styles from './Navbar.module.css';
 // GNB 역할
 const Navbar = () => {
-  return <div>네비게이션 바가 들어갑니다</div>;
+  const ctx = useContext(AuthContext);
+  const loginStatus = ctx.isLoggedIn;
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src='#' alt='logo' />
+      </div>
+      <div>
+        {loginStatus ? (
+          <Link to='/'>
+            <button className={styles.navbtn}>마이페이지</button>
+          </Link>
+        ) : (
+          <Link to='/'>
+            <button className={styles.navbtn}>회원가입/로그인</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
