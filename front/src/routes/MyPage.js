3c13d672b5dfffca5416f3f6d472b6984052ea57
deeun <jiwon94k@gmail.com>
2022-09-15 11:49:29 +0900

    Refactor : My Page 컴포넌트 분리

--- front/src/routes/MyPage.js
+++ front/src/routes/MyPage.js
@@ -1,25 +1,19 @@
 import React, { useState, useEffect, useContext } from "react";
-import MyEatketList from "../component/MyPage/MyEatketList";
-import MyReviews from "../component/MyPage/MyReviews";
-import styles from "./MyPage.module.css";
-import axios, { instance } from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import { instance } from "../api/axios";
+
 import Profile from "../component/MyPage/Profile";
 import AuthContext from "../store/auth-context";
-import { useNavigate } from "react-router-dom";
+import Tabs from "../component/MyPage/Tabs";
+
+import styles from "./MyPage.module.css";
 
 const MyPage = () => {
-  const [tabselect, setTabselect] = useState(false);
   const [user, setUser] = useState("");
   const [datafetch, setDatafetch] = useState(false);
   const [isUpdated, setIsUpdated] = useState(false);
 
   const ctx = useContext(AuthContext);
-  const showReviewsHandler = () => {
-    setTabselect();
-  };
-  const showEatketListHandler = () => {
-    setTabselect(true);
-  };
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -42,37 +36,11 @@ const MyPage = () => {
       });
   }, [isUpdated]);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setTokenUpdate(!tokenUpdate);
-  //   }, 1000 * 5);
-  // }, [tokenUpdate]);
-
   return (
     datafetch && (
       <div className={styles.wrapper}>
         <Profile user={user} updateHandler={() => setIsUpdated(!isUpdated)} />
-        <nav id="mypage_tabmenu" className={styles.tabmenu}>
-          <div className={styles.tabbar}>
-            <div className={tabselect ? styles.tab : styles.tabactive}>
-              <button
-                className={tabselect ? styles.tab : styles.tabactive}
-                onClick={showReviewsHandler}
-              >
-                작성 리뷰
-              </button>
-            </div>
-            <div className={tabselect ? styles.tabactive : styles.tab}>
-              <button
-                className={tabselect ? styles.tabactive : styles.tab}
-                onClick={showEatketListHandler}
-              >
-                먹킷 리스트{" "}
-              </button>
-            </div>
-          </div>
-        </nav>
-        <div>{tabselect ? <MyEatketList /> : <MyReviews />}</div>
+        <Tabs />
       </div>
     )
   );
