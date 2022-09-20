3f3f4f21852753581ea29a661a4a7c32ec5af5c0
deeun <jiwon94k@gmail.com>
2022-09-20 14:43:36 +0900

    Feat : 리뷰 수정 기능 작업 중

--- front/src/component/MyPage/MyReview/ReviewEdit.js
+++ front/src/component/MyPage/MyReview/ReviewEdit.js
@@ -3,6 +3,7 @@ import Modal from "../../UI/Modal";
 import Button from "../../UI/Button";
 import Input from "../../UI/Input";
 import styles from "./ReviewEdit.module.css";
+import { instance } from "../../../api/axios";
 
 const ReviewEdit = (props) => {
   const [editedTitle, setEditedTitle] = useState(props.reviewTitle);
@@ -20,19 +21,61 @@ const ReviewEdit = (props) => {
     setEditedContent(e.target.value);
   };
 
+  const updateHandler = () => {
+    props.updateHandler();
+  };
+
+  const reviewEditSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("accessToken");
+    const profile = { reviewTitle: editedTitle, reviewContent: editedContent };
+    instance
+      .put(
+        `/review/${props.reviewIndex}/auth/updateReview`,
+        JSON.stringify(profile),
+        {
+          headers: { "Content-Type": "application/json", Authorization: token },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        updateHandler();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Modal>
       <div className={styles.wrapper}>
-        <label htmlFor="editedTitle">한줄평</label>
-        <Input id="editedTitle" type="text" value={editedTitle} onChange={titleEditHandler} />
-        <br />
-        <br />
-        <label htmlFor="editedContent">상세리뷰</label><br/>
-        <textarea id="editedContent" type="comment" value={editedContent} className={styles.textarea} cols={47} onChange={contentEditHandler}/>
-        <div className={styles.buttonArea}>
-          <Button style={{ marginRight: "6px" }}>저장하기</Button>
-          <Button onClick={reviewEditClose}>닫기</Button>
-        </div>
+        <form onSubmit={reviewEditSubmit}>
+          <label htmlFor="editedTitle">한줄평</label>
+          <Input
+            id="editedTitle"
+            type="text"
+            value={editedTitle}
+            onChange={titleEditHandler}
+          />
+          <br />
+          <br />
+          <label htmlFor="editedContent">상세리뷰</label>
+          <br />
+          <textarea
+            id="editedContent"
+            type="comment"
+            value={editedContent}
+            className={styles.textarea}
+            cols={47}
+            onChange={contentEditHandler}
+          />
+          <div className={styles.buttonArea}>
+            <Button type="submit" style={{ marginRight: "6px" }}>
+              저장하기
+            </Button>
+            <Button onClick={reviewEditClose}>닫기</Button>
+          </div>
+        </form>
       </div>
     </Modal>
   );
