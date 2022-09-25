3ccc52cf68b0a04e90574d424a2f36711b0e2589
woowsnu <woowsnu@gmail.com>
2022-09-25 20:24:29 +0900

    Design : 푸터 디자인, 링크 수정
    - 깃허브 추가 연결

--- front/src/component/Layout/Footer.js
+++ front/src/component/Layout/Footer.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import styles from './Footer.module.css';
-import { FaHome } from 'react-icons/fa';
+import { FaHome, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
-  return <div className={styles.footer}>
-    <div>
-      <div>㈜짐승들 </div>
-      <div><a href='https://foam-tendency-a06.notion.site/RE-STAURANT-ee29783ffaba4b6593e290616239fc21' target='_blank'> <FaHome /></a></div>
+  return (
+    <div className={styles.footer}>
+      <div>
+        <div style={{paddingBottom: '10px'}}>㈜짐승들 </div>
+        <div>Copyright ⓒ 2022 Beasts</div>
+      </div>
+      <div>
+        <a
+          href='https://foam-tendency-a06.notion.site/RE-STAURANT-ee29783ffaba4b6593e290616239fc21'
+          target='_blank'
+        >
+          <FaHome style={{fontSize: '24px', paddingRight: '12px'}}/>
+        </a>
+        <a href='https://github.com/woowsnu/Re-staurant'
+          target='_blank'>
+        <FaGithub style={{fontSize: '24px'}}/>
+        </a>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Footer;
