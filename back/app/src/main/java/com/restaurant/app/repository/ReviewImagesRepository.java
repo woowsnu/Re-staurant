d6b9a0069024bc42b3747c9d8b61f0344b5b89ce
nikeisme <nikeisme827@gmail.com>
2022-09-26 15:08:55 +0900

    Fix : 깃 머지 오류

--- back/app/src/main/java/com/restaurant/app/repository/ReviewImagesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.app.repository;
-
-import com.restaurant.app.model.ReviewImages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewImagesRepository extends JpaRepository<ReviewImages, Long> {
-}
--- back/app/src/main/java/com/restaurant/app/repository/VotedKeywordsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.app.repository;
-
-import com.restaurant.app.model.VotedKeywords;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VotedKeywordsRepository extends JpaRepository<VotedKeywords,Long> {
-}
