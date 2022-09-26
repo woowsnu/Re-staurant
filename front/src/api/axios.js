57dbcbc9036a2176c90fb098b35a9d52d5509c07
kanggun11@naver.com <kanggun9908@gmail.com>
2022-09-26 23:17:57 +0900

    Fix: front EC2.ver  down_grade

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  // baseURL: 'http://localhost:8080',
-  baseURL: 'http://43.200.202.164:8080',
+ // baseURL: 'http://localhost:8080',
+ baseURL: 'http://43.200.202.164:8080',
 });
 
 instance.interceptors.response.use(
