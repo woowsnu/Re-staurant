e46ff88895033d6b93a93470f82e2bebd5061e0b
nikeisme <nikeisme827@gmail.com>
2022-09-26 14:46:59 +0900

    Fix : merge 오류

--- front/src/component/UI/Topbar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Topbar.module.css'
-
-const Topbar = (props) => {
-  return <div className={styles.container}>{props.children}</div>;
-};
-
-export default Topbar;
