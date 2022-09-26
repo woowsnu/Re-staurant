59417eab66dafd3f1a7db440aaa15ea2e41e0cd8
nikeisme <nikeisme827@gmail.com>
2022-09-26 15:55:22 +0900

    Fix : 깃 머지 오류

--- back/app/src/main/java/com/restaurant/app/model/Follow.java
+++ back/app/src/main/java/com/restaurant/app/model/Follow.java
@@ -1,11 +1,8 @@
 package com.restaurant.app.model;
-
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-
 import javax.persistence.*;
 import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Setter
@@ -13,25 +10,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Follow {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long followIndex;
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = User.class)
     @JoinColumn(name="following_user_index")
     private User followingUser;
 
-    @ManyToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = User.class)
     @JoinColumn(name="follwed_user_index")
     private User followedUser;
+
+    @Column(name="removed")
+    private Integer removed;
 
     @CreatedDate
     private LocalDateTime createDate;
 
-    public Follow(User followingUser, User followedUser) {
-        this.followingUser = followingUser;
-        this.followedUser = followedUser;
-        this.createDate = LocalDateTime.now();
+    public Follow(User followingUser, User followedUser, Integer removed) {
+            this.followingUser = followingUser;
+            this.followedUser = followedUser;
+            this.removed = removed;
+            this.createDate = LocalDateTime.now();
+        }
     }
-}
